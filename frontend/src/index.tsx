eb3806f678203a57e2fef9438921cef527f2c4c1
Kento75 <kento2github@gmail.com>
2018-10-20 17:53:11 +0900

    Fix TSlint disable one line

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import "normalize.css"
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+// tslint:disable-next-line
 import "semantic-ui-css/semantic.min.css"
 
 import { Hello } from "./components/Hello"
